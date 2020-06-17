72db8ca7ed8c18a5775b0515abb6e3bf1ebbd31d
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-17 11:26:32 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { MenuComponent } from './components/menu/menu.component';
+import { CadastroSegurosComponent } from './components/cadastro-seguros/cadastro-seguros.component';
+import { ListarSegurosComponent } from './components/listar-seguros/listar-seguros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastroSegurosComponent,
+    ListarSegurosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
