86eafeca4385087aaa606d48d59a9e5b58b026f6
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-19 15:15:59 -0300

    <feature> - settings working offline

--- src/app/services/seguro.service.ts
+++ src/app/services/seguro.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Seguro } from '../models/Seguro';
-
+import { Observable } from 'rxjs';
+import { OnlineOfflineService } from './online-offline.service';
+ 
 @Injectable({
   providedIn: 'root'
 })
@@ -9,11 +11,42 @@ export class SeguroService {
 
   private API_SEGUROS = 'http://localhost:9000/';
 
-  constructor(private http: HttpClient) { }
 
-  cadastrar(seguro: Seguro) {
-    this.http.post(this.API_SEGUROS + `api/seguros`, seguro).subscribe(() => { console.log('Seguro cadastrado com sucesso') }, (err) => {
-      console.log("deu erro,", err);
+  constructor(private http: HttpClient,
+    private onlineOfflineSerivce: OnlineOfflineService) {
+    this.ouvirStatusConexao();
+  }
+
+
+  private salvarAPI(seguro: Seguro) {
+    this.http.post(this.API_SEGUROS + 'api/seguros', seguro).subscribe(() => {
+      alert('Seguro foi cadastrado com sucesso')
+    }, error => {
+      alert('Erro ao cadastrar o seguro')
     });
+  }
+
+  ouvirStatusConexao() {
+    this.onlineOfflineSerivce.statusConexao.subscribe(online => {
+      if (online) {
+        console.log("status online");
+      } else {
+        console.log("estou offline");
+      }
+    });
+  }
+
+  public cadastrar(seguro: Seguro) {
+    this.http.post(this.API_SEGUROS + `api/seguros`, seguro).subscribe(() => {
+      if (this.onlineOfflineSerivce.isOnline) {
+        this.salvarAPI(seguro);
+      } else {
+        console.log('salvar seguro no banco local');
+      }
+    });
+  }
+
+  listar(): Observable<Seguro[]> {
+    return this.http.get<Seguro[]>(this.API_SEGUROS + 'api/seguros');
   }
 }
